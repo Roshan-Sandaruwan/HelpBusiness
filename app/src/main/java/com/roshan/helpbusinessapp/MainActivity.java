0327fb4280b4a27cefc70129fad9ada8b348ebb9
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-06-06 16:55:07 +0530

    first commit

---
+++ app/src/main/java/com/roshan/helpbusinessapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.roshan.helpbusinessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
