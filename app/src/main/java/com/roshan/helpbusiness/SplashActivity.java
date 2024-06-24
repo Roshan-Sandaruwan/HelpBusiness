633e4f34b918e7fe0844acad18c597ba5506a306
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-06-24 15:20:42 +0530

    splash screen update

--- app/src/main/java/com/roshan/helpbusiness/SplashActivity.java
+++ app/src/main/java/com/roshan/helpbusiness/SplashActivity.java
@@ -2,7 +2,11 @@ package com.roshan.helpbusiness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+//        ---splash screen login and Register buttons---
+
+        Button spLoginBtn = findViewById(R.id.spLoginBtn);
+        spLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+            }
+        });
+
+        Button spRegisterBtn = findViewById(R.id.spRegisterBtn);
+        spRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this,RegisterActivity.class));
+            }
+        });
+
+
+
+
+
+
     }
 }
