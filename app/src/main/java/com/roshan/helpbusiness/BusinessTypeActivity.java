17a31231d8197fb4f4845bfb9a2521c80c0d3032
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-06-27 00:59:13 +0530

    business type and large scale update

---
+++ app/src/main/java/com/roshan/helpbusiness/BusinessTypeActivity.java
@@ -0,0 +1,14 @@
+package com.roshan.helpbusiness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusinessTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_type);
+    }
+}
---
+++ app/src/main/java/com/roshan/helpbusiness/LargeScaleActivity.java
@@ -0,0 +1,14 @@
+package com.roshan.helpbusiness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LargeScaleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_large_scale);
+    }
+}
