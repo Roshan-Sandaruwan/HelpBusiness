bd185a939c9b5d75c1b70c61019c0b9643e4825c
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-06-30 01:55:21 +0530

    products data insert successful

--- app/src/main/java/com/roshan/helpbusiness/Activity/CreateProductActivity.java
+++ app/src/main/java/com/roshan/helpbusiness/Activity/CreateProductActivity.java
@@ -5,16 +5,88 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.roshan.helpbusiness.R;
 
 public class CreateProductActivity extends AppCompatActivity {
 
+    EditText shortTitle, productTitle,productDescription,productOwnerPhone,productOwnerEmail,productOwnerLocation,oneProductPrice,allProductPrice;
+    Button insert,view;
+    DBHelper DB;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_product);
+
+//        --- product create ---
+
+        shortTitle = (EditText) findViewById(R.id.shortTitle);
+        productTitle = (EditText) findViewById(R.id.productTitle);
+        productDescription =(EditText) findViewById(R.id.productDescription);
+        productOwnerPhone =(EditText) findViewById(R.id.productOwnerPhone);
+        productOwnerEmail =(EditText) findViewById(R.id.productOwnerEmail);
+        productOwnerLocation =(EditText) findViewById(R.id.productOwnerLocation);
+        oneProductPrice =(EditText) findViewById(R.id.oneProductPrice);
+        allProductPrice =(EditText) findViewById(R.id.allProductPrice);
+
+        insert = findViewById(R.id.insertBtn);
+        view = findViewById(R.id.viewBtn);
+
+        DB = new DBHelper(this);
+
+
+//        --- VIEW BUTTON ---
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CreateProductActivity.this,LargeScaleActivity.class));
+            }
+        });
+
+//        ---PRODUCT DETAILS INSERT TO THE DB
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String shortTitleTxt = shortTitle.getText().toString();
+                String productTitleTxt = productTitle.getText().toString();
+                String productDescriptionTxt = productDescription.getText().toString();
+                String productOwnerPhoneTxt = productOwnerPhone.getText().toString();
+                String productOwnerEmailTxt = productOwnerEmail.getText().toString();
+                String productOwnerLocationTxt = productOwnerLocation.getText().toString();
+                String oneProductPriceTxt = oneProductPrice.getText().toString();
+                String allProductPriceTxt = allProductPrice.getText().toString();
+
+                Boolean checkinsertdata = DB.insertProductData(
+                        shortTitleTxt,
+                        productTitleTxt,
+                        productDescriptionTxt,
+                        productOwnerPhoneTxt,
+                        productOwnerEmailTxt,
+                        productOwnerLocationTxt,
+                        oneProductPriceTxt,
+                        allProductPriceTxt
+                        );
+
+                if(checkinsertdata==true){
+                    Toast.makeText(CreateProductActivity.this, "New Product Added", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(CreateProductActivity.this, "Product is not added", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+//        --- BACK BUTTON ---
 
         ImageView productCreateBackBtn = findViewById(R.id.productCreateBackBtn);
         productCreateBackBtn.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +95,6 @@ public class CreateProductActivity extends AppCompatActivity {
                 startActivity(new Intent(CreateProductActivity.this,SmallScaleActivity.class));
             }
         });
+
     }
 }
--- app/src/main/java/com/roshan/helpbusiness/Activity/DBHelper.java
+++ app/src/main/java/com/roshan/helpbusiness/Activity/DBHelper.java
@@ -13,21 +13,43 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String DBNAME = "Helpbusiness.db";
 
     public DBHelper(Context context) {
-        super(context, "Helpbusiness.db", null, 1);
+        super(context, "Helpbusiness.db", null, 2);
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE users(username TEXT  PRIMARY KEY , email TEXT, password TEXT)"); //-- create table
+
+//        ---CREATE USER TABLE--
+
+        db.execSQL("CREATE TABLE users(username TEXT  PRIMARY KEY , email TEXT, password TEXT)");
+
+//        ---CREATE PRODUCT TABLE---
+
+        db.execSQL("CREATE TABLE products(id INTEGER PRIMARY KEY AUTOINCREMENT, shortTitle TEXT, productTitle TEXT, productDescription TEXT, productOwnerPhone TEXT, productOwnerEmail TEXT, productOwnerLocation TEXT, oneProductPrice TEXT, allProductPrice TEXT)");
+
+
+
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+//        ---USER TABLE---
+
         db.execSQL("DROP TABLE IF EXISTS users");
 
+//        ---PRODUCT TABLE---
+
+        db.execSQL("DROP TABLE IF EXISTS products");
+
+
+        onCreate(db); //--error after add
+
     }
+
+//    ---USER TABLE---
 
     public Boolean insertData(String username, String email, String password){
         SQLiteDatabase db = this.getWritableDatabase();
@@ -64,6 +86,50 @@ public class DBHelper extends SQLiteOpenHelper {
         else
             return false;
     }
+
+
+//    ---PRODUCT TABLE---
+
+    public Boolean insertProductData(String shortTitle,
+                                     String productTitle,
+                                     String productDescription,
+                                     String productOwnerPhone,
+                                     String productOwnerEmail,
+                                     String productOwnerLocation,
+                                     String oneProductPrice,
+                                     String allProductPrice){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("shortTitle",shortTitle);
+        contentValues.put("productTitle",productTitle);
+        contentValues.put("productDescription",productDescription);
+        contentValues.put("productOwnerPhone",productOwnerPhone);
+        contentValues.put("productOwnerEmail",productOwnerEmail);
+        contentValues.put("productOwnerLocation",productOwnerLocation);
+        contentValues.put("oneProductPrice",oneProductPrice);
+        contentValues.put("allProductPrice",allProductPrice);
+
+        long result = db.insert("products", null,contentValues);
+
+        if (result==-1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getdata(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from products", null);
+        return cursor;
+    }
+
+
+
+
+
+
+
 
 
 }
